b08dfc9f4f80546328cbd2543da35a105dc935d8
joshuapbritz <joshua@phiresky.com>
2017-05-03 12:58:49 +0200

    Initial Commit

---
+++ js/init.js
@@ -0,0 +1,8 @@
+
+window.onload = function () {
+    counterUp('counter-up', 2500, 1000);
+    timeR(1000);
+    counterDown('count-down', '17', 'June', '2017', '00:00', 'Expired');
+}
+
+preloader('preloader-wrapper', 0);
